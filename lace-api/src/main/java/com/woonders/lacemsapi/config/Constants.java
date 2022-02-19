5318330c965151d2e6051071d97bbd4fe479d3d7
Alessio De Vincentis <Alessio.DeVincentis@nttdata.com>
2022-02-19 14:10:19 +0100

    clean

--- lace-api/src/main/java/com/woonders/lacemsapi/config/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.woonders.lacemsapi.config;
-
-/**
- * Created by emanuele on 29/12/16.
- */
-public class Constants {
-	public static final String EMPTY_DEFAULT_DB_NAME = "emptydefault";
-}
--- lace-api/src/main/java/com/woonders/lacemsapi/controller/ControllerConstants.java
+++
@@ -1,9 +0,0 @@
-package com.woonders.lacemsapi.controller;
-
-/**
- * Created by ema98 on 8/9/2017.
- */
-public class ControllerConstants {
-
-    public static final String V1 = "v1";
-}
